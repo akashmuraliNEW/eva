531402d95164f06546105aa4e70f17dd2e5cfbd1
Akashvro <90610484+Akashvro@users.noreply.github.com>
2022-04-06 07:50:50 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -134,6 +134,8 @@ async def advantage_spoll_choker(bot, query):
         if files:
             k = (movie, files, offset, total_results)
             await auto_filter(bot, query, k)
+            await asyncio.sleep(30)
+            await k.delete()
         else:
             k = await query.message.edit('This Movie is not found in my database ,Reasons - Movie not released ,NO cam prints,HD files mathreme bot ill upload chyu vro . contact admin if its an old movie to add in database @HELL_GaM')
             await asyncio.sleep(30)
