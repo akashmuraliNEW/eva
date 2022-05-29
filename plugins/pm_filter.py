7167c4835a597d4e53568b2c5988a7789b5cd1f5
Akassh <100897807+akashmuraliNEW@users.noreply.github.com>
2022-05-29 14:38:00 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -723,6 +723,7 @@ async def auto_filter(client, msg, spoll=False):
     else:
         await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
     if spoll:
+        await asyncio.sleep(10)
         await msg.message.delete()
 
 
