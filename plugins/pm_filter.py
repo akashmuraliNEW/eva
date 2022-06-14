c53db99c5daa094fcd8b7e3031d9746e5001eddd
Akassh <100897807+akashmuraliNEW@users.noreply.github.com>
2022-06-14 21:37:17 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -804,8 +804,6 @@ async def manual_filters(client, message, text=False):
                     if fileid == "None":
                         if btn == "[]":
                       msg = await client.send_message(group_id, reply_text, disable_web_page_preview=True, reply_to_message_id=reply_id)
-                            await asyncio.sleep(10)
-                            await msg.delete()
                         else:
                             button = eval(btn)
                             await client.send_message(
