ef0069133911ae4d3680560f2e50524638f8f1e8
Akassh <100897807+akashmuraliNEW@users.noreply.github.com>
2022-06-15 21:38:08 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1,4 +1,4 @@
-# Kanged From @TroJanZheX
+   # Kanged From @TroJanZheX
 import asyncio
 import re
 import ast
@@ -722,13 +722,13 @@ async def auto_filter(client, msg, spoll=False):
         except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
             pic = imdb.get('poster')
             poster = pic.replace('.jpg', "._V1_UX360.jpg")
-           await message.reply_photo(photo=poster, caption=cap[:1024], reply_markup=InlineKeyboardMarkup(btn))
+            await message.reply_photo(photo=poster, caption=cap[:1024], reply_markup=InlineKeyboardMarkup(btn))
         except Exception as e:
             logger.exception(e)
             await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
     else:
-         await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
-    if spoll:  
+        await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
+    if spoll:
         await msg.message.delete()
 
 
